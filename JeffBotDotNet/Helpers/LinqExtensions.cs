a00f385784ed193c552d0142feb22be4d6c46ce8
Óskar Sandholt <oskarjorgen@gmail.com>
2021-10-06 18:48:47 +0000

    Added linq helper to find all indexes of char in string.

---
+++ JeffBotDotNet/Helpers/LinqExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeffBotDotNet.Helpers
+{
+    public static class LinqExtensions
+    {
+        public static IEnumerable<int> AllIndexesOf(this string str, string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentException("the string to find may not be empty", nameof(value));
+            for (int index = 0; ; index += value.Length)
+            {
+                index = str.IndexOf(value, index);
+                if (index == -1)
+                    break;
+                yield return index;
+            }
+        }
+    }
+}
