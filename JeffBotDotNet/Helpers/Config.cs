2a7a3e7fd146dd62c5ffb7d8ae95475b742e66f2
Óskar Sandholt <oskarjorgen@gmail.com>
2021-10-22 22:26:37 +0000

    more config testing

--- JeffBotDotNet/Helpers/Config.cs
+++ JeffBotDotNet/Helpers/Config.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,10 +13,17 @@ namespace JeffBotDotNet.Helpers
     {
         public static ConfigModel GetConfig()
         {
-            var path = $"{Path.Combine(Environment.CurrentDirectory.Split(Path.DirectorySeparatorChar).SkipLast(3).ToArray())}{Path.DirectorySeparatorChar}Config.json";
+            //var path = $"{Path.Combine(Environment.CurrentDirectory.Split(Path.DirectorySeparatorChar).SkipLast(3).ToArray())}{Path.DirectorySeparatorChar}Config.json";
+            var path = Environment.CurrentDirectory.Split(Path.DirectorySeparatorChar).Where(x => true);
+            
+            if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                path = path.SkipLast(3);
+            }
+
             Console.WriteLine($"FULL: {Path.Combine(Environment.CurrentDirectory.Split(Path.DirectorySeparatorChar))}");
-            Console.WriteLine($"PARTIAL: {Path.Combine(Environment.CurrentDirectory.Split(Path.DirectorySeparatorChar).SkipLast(3).ToArray())}");
-            return JsonConvert.DeserializeObject<ConfigModel>(File.ReadAllText(path));
+            Console.WriteLine($"PARTIAL: {Path.Combine(path.ToArray())}");
+            return JsonConvert.DeserializeObject<ConfigModel>(File.ReadAllText($"{Path.Combine(Path.Combine(path.ToArray()), "Config.json")}"));
         }
     }
 
