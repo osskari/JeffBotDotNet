2be0f2cabcd5dc6f42a15012c86d441e45c83145
Óskar Sandholt <oskarjorgen@gmail.com>
2021-10-22 22:57:49 +0000

    Added unit UnitCoverterModule which allows for unit conversion in chat

---
+++ JeffBotDotNet/Modules/UnitConverterModule.cs
@@ -0,0 +1,42 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeffBotDotNet.Modules
+{
+    [Group("convert")]
+    [Summary("Unit conversion commands for conversations between people from different places")]
+    public class UnitConverterModule : ModuleBase<SocketCommandContext>, IJeffBotModule
+    {
+        [Command("ctof")]
+        [Alias("celsiustofahrenheit", "celsius2fahrenheit", "c2f")]
+        [Summary("converts celsius to fahrenheit")]
+        public async Task CToFAsync([Summary("celsius value")] double c)
+        {
+            await ReplyAsync($"{c}°C => {(c * 9 / 5) + 32:F2}°F");
+        }
+
+        [Command("ftoc")]
+        [Alias("fahrenheittocelsius", "fahrenheit2celsius", "f2c")]
+        [Summary("converts fahrenheit to celsius")]
+        public async Task FToCAsync([Summary("fahrenheit value")] double f)
+        {
+            await ReplyAsync($"{f}°F => {((f - 32) * 5 / 9):F2}°C");
+        }
+
+        [Command("help")]
+        [Alias("-h", "h")]
+        [Summary("Prints out instructions on how to use the module")]
+        public async Task HelpAsync()
+        {
+            var a = new Discord.EmbedBuilder();
+            a.AddField("name", "value");
+            a.AddField("name2", "value2", true);
+            await ReplyAsync(embed: a.Build());
+            //throw new NotImplementedException();
+        }
+    }
+}
